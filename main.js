83965a0907b09689231bde18f54776f5069af048
“Willie” <willie.williams@codeimmersives.com>
2021-02-19 08:06:34 -0500

    finished

---
+++ main.js
@@ -0,0 +1,42 @@
+function greeting(string) {
+    return string + '???';
+};
+
+const greetingresult = greeting('Hello Willie');
+
+greetingresult;
+
+function addTwo(string1, string2) {
+    return string1 + ' ' + string2;
+};
+
+const addTwoResult = addTwo('Hey', 'there');
+addTwoResult;
+
+function subtract(num1, num2, num3) {
+    return num1 - num2 - num3;
+}
+
+const subtractResult = subtract(10, 1, 2);
+subtractResult;
+
+function subtractbackwards(num1, num2, num3) {
+    return num3 - num2 - num1;
+};
+
+const subtractBackwardsResult = subtractbackwards(10, 1, 2);
+subtractBackwardsResult;
+
+function triplestring(string) {
+    return string + string + string;
+};
+
+const tripleStringResult = triplestring('Willie D. ')
+tripleStringResult;
+
+function squareroot(num){
+    return Math.sqrt(num);
+}
+
+const squareRootResult = squareroot(78);
+squareRootResult;
